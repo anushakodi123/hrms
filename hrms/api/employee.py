48c883da6a80ef6ae2ced6f6e9d6b8e732885fc4
anushakodi01234 <anushakodi01234@gmail.com>
2024-06-10 08:56:30 +0530

    added graphql for employee

--- hrms/api/employee.py
+++ hrms/api/employee.py
@@ -1,7 +1,8 @@
 import fastapi as fa
-from hrms.svc import service
+from hrms.svc import employee
 import typing as ty
 import pydantic as pyd
+from hrms.graphql import sample
 
 
 class Employee(pyd.BaseModel):
@@ -14,21 +15,21 @@ router = fa.APIRouter()
 
 
 @router.get("")
-def list(employee_svc: ty.Annotated[service.Employee, fa.Depends(service.Employee, use_cache=True)]):
+def list(employee_svc: ty.Annotated[employee.Employee, fa.Depends(employee.Employee, use_cache=True)]):
     return employee_svc.list()
 
 @router.get("/{id}")
-def get(id: int, employee_svc: ty.Annotated[service.Employee, fa.Depends(service.Employee)]):
+def get(id: int, employee_svc: ty.Annotated[employee.Employee, fa.Depends(employee.Employee)]):
     return employee_svc.get(id)
 
 @router.post("")
-def add(employee_svc: ty.Annotated[service.Employee, fa.Depends(service.Employee)], employee: Employee):
+def add(employee_svc: ty.Annotated[employee.Employee, fa.Depends(employee.Employee)], employee: Employee):
     return employee_svc.add(employee)
 
 @router.put("/{id}")
-def edit(id: int, employee_svc: ty.Annotated[service.Employee, fa.Depends(service.Employee)], employee: Employee):
+def edit(id: int, employee_svc: ty.Annotated[employee.Employee, fa.Depends(employee.Employee)], employee: Employee):
     return employee_svc.edit(id, employee)
 
 @router.delete("/{id}")
-def remove(id: int, employee_svc: ty.Annotated[service.Employee, fa.Depends(service.Employee)]):
+def remove(id: int, employee_svc: ty.Annotated[employee.Employee, fa.Depends(employee.Employee)]):
     return employee_svc.remove(id)
--- hrms/main.py
+++ hrms/main.py
@@ -1,7 +1,11 @@
 import fastapi as fa
 from hrms.api import ping, employee
+from hrms.graphql import employeee, sample
+
 
 app = fa.FastAPI()
 
 app.include_router(ping.router, prefix="/ping")
 app.include_router(employee.router, prefix="/employee")
+app.include_router(sample.graphql_app, prefix="/graphql")
+app.include_router(employeee.graphql_schema2, prefix="/graphql1")
